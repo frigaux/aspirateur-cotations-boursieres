a3b2e83fc69e2cf074de8f2ddc4c85f1f0885d80
fabien <fabien.rigaux@free.fr>
2025-01-29 19:31:14 +0100

    Start dev

--- src/main/kotlin/fr/fabien/aspirateur/cotations/ApplicationAspirateur.kt
+++ src/main/kotlin/fr/fabien/aspirateur/cotations/ApplicationAspirateur.kt
@@ -1,27 +1,23 @@
 package fr.fabien.aspirateur.cotations
 
-import org.springframework.batch.core.Job
-import org.springframework.batch.core.JobExecution
-import org.springframework.batch.core.JobParametersBuilder
-import org.springframework.batch.core.launch.JobLauncher
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
-import org.springframework.context.ApplicationContext
-import java.time.LocalDateTime
 
-// TODO : test not running + datasource test + mvn spring-boot:run + CommandLineJobRunner
+// TODO : mvn spring-boot:run + CommandLineJobRunner
+//@SpringBootApplication
+//class ApplicationAspirateur(val jobLauncher: JobLauncher, val applicationContext: ApplicationContext) : CommandLineRunner {
+//    override fun run(vararg args: String?) {
+//        val jobMajLibelles: Job = applicationContext.getBean("jobMajLibelles") as Job
+//        val jobParameters = JobParametersBuilder()
+//            .addLocalDateTime("now", LocalDateTime.now())
+//            .toJobParameters()
+//        val jobExecution: JobExecution = jobLauncher.run(jobMajLibelles, jobParameters);
+//    }
+//}
+
 @SpringBootApplication
-class ApplicationAspirateur(val jobLauncher: JobLauncher, val applicationContext: ApplicationContext) : CommandLineRunner {
-    override fun run(vararg args: String?) {
-        val jobMajLibelles: Job = applicationContext.getBean("jobMajLibelles") as Job
-        val jobParameters = JobParametersBuilder()
-            .addLocalDateTime("now", LocalDateTime.now())
-            .toJobParameters()
-        val jobExecution: JobExecution = jobLauncher.run(jobMajLibelles, jobParameters);
-    }
-}
+class ApplicationAspirateur
 
 // -Dspring.config.name=configuration -Dspring.profiles.active=dev
 fun main(args: Array<String>) {
--- src/test/kotlin/fr/fabien/aspirateur/cotations/TestConfigurationAspirateur.kt
+++ src/test/kotlin/fr/fabien/aspirateur/cotations/TestConfigurationAspirateur.kt
@@ -1,42 +1,35 @@
 package fr.fabien.aspirateur.cotations
 
+import fr.fabien.aspirateur.cotations.repository.LibelleRepository
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.batch.core.*
 import org.springframework.batch.test.JobLauncherTestUtils
-import org.springframework.batch.test.JobRepositoryTestUtils
 import org.springframework.batch.test.context.SpringBatchTest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
-import kotlin.test.AfterTest
 
 @ActiveProfiles("test")
 @SpringBatchTest
 @SpringBootTest
-//@SpringBootTest(classes = [ConfigurationAspirateur::class])
 class TestConfigurationAspirateur {
 
     @Autowired
     private val jobLauncherTestUtils: JobLauncherTestUtils? = null
 
     @Autowired
-    private val jobRepositoryTestUtils: JobRepositoryTestUtils? = null
-
-    @Autowired
     private val jobMajLibelles: Job? = null
 
-    @AfterTest
-    fun tearDown() {
-        jobRepositoryTestUtils?.removeJobExecutions()
-    }
+    @Autowired
+    private val libelleRepository: LibelleRepository? = null
 
     @Test
     @Throws(Exception::class)
     fun launchJob_WhenJobEnds_ThenStatusCompleted() {
-        jobLauncherTestUtils?.setJob(jobMajLibelles!!) // job is launch here ?!?
-        val jobExecution: JobExecution = jobLauncherTestUtils?.launchJob()!!
-        // TODO : check table not empty !
+        jobLauncherTestUtils!!.setJob(jobMajLibelles!!) // job is launch here ?!?
+        val jobExecution: JobExecution = jobLauncherTestUtils.launchJob()
         Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.exitStatus)
+        Assertions.assertTrue(libelleRepository!!.count() > 0)
     }
 }
