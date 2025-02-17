696a8af0052aac47b7cfed8f39dca80b5f844a39
fabien <fabien.rigaux@free.fr>
2025-02-17 16:08:27 +0100

    Start dev

--- launchJobs.sh
+++ launchJobs.sh
@@ -2,26 +2,27 @@
 
 date=$(date +"%d/%m/%Y")
 echo $date
+dir=target
 
-java -Dspring.profiles.active=dev -DJOB_NAME=jobMajAbcLibelles -DDATE=$date -jar target/aspirateur-cotations-boursieres-0.0.1-SNAPSHOT.jar
+java -Dspring.profiles.active=dev -DJOB_NAME=jobMajAbcLibelles -DDATE=$date -jar $dir/aspirateur-cotations-boursieres-0.0.1-SNAPSHOT.jar
 
 if [ $? != 0 ]; then
   exit 1
 fi
 
-java -Dspring.profiles.active=dev -DJOB_NAME=jobMajAbcCotations -DDATE=$date -jar target/aspirateur-cotations-boursieres-0.0.1-SNAPSHOT.jar
+java -Dspring.profiles.active=dev -DJOB_NAME=jobMajAbcCotations -DDATE=$date -jar $dir/aspirateur-cotations-boursieres-0.0.1-SNAPSHOT.jar
 
 if [ $? != 0 ]; then
   exit 2
 fi
 
-java -Dspring.profiles.active=dev -DJOB_NAME=jobAbcToValeurCours -DDATE=$date -jar target/aspirateur-cotations-boursieres-0.0.1-SNAPSHOT.jar
+java -Dspring.profiles.active=dev -DJOB_NAME=jobAbcToValeurCours -DDATE=$date -jar $dir/aspirateur-cotations-boursieres-0.0.1-SNAPSHOT.jar
 
 if [ $? != 0 ]; then
   exit 3
 fi
 
-java -Dspring.profiles.active=dev -DJOB_NAME=jobCalculerMoyennes -DDATE=$date -jar target/aspirateur-cotations-boursieres-0.0.1-SNAPSHOT.jar
+java -Dspring.profiles.active=dev -DJOB_NAME=jobCalculerMoyennes -DDATE=$date -jar $dir/aspirateur-cotations-boursieres-0.0.1-SNAPSHOT.jar
 
 if [ $? != 0 ]; then
   exit 4
